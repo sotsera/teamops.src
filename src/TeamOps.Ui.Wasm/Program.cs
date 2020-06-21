2eb6d0bcf37a269b4184df092e68f63adf9258b1
ghidello <ghidello@users.noreply.github.com>
2020-06-21 15:46:45 +0200

    testing azure b2c

--- src/TeamOps.Ui.Wasm/Program.cs
+++ src/TeamOps.Ui.Wasm/Program.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TeamOps.Ui.Wasm
 {
@@ -14,6 +18,11 @@ namespace TeamOps.Ui.Wasm
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+            });
 
             await builder.Build().RunAsync();
         }
